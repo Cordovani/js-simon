d70ad632e0efb3a8cc7b43b32b5f153acdb4dbda
Cordovani <lor.cordovani@gmail.com>
2021-04-21 15:24:10 +0200

    struttura del progetto

---
+++ script.js
@@ -0,0 +1,9 @@
+// 3 array, numeriGenerati, numeriCorretti, numeriErrati
+
+// genero i 5 numeri da saalvare in numeriGenerati e li mostro all'utente
+
+// faccio partire un timer di 30 secondi
+
+// chiedo di inserire i dati all'utente
+
+//mostro il risultato da numeriCorretti e numeriErrati
